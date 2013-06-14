ef2f8db9dd88d3de18d39d793c30352b3a409a37
Antonio Sejas <antonio@sejas.es>
2013-06-15 00:28:14 +0200

    funcionando, a falta de analytics

--- bucletube.js
+++ bucletube.js
@@ -1,13 +1,30 @@
-alert('Cargada');
+
   chrome.browserAction.onClicked.addListener(function(tab) {
-  	alert('click');
 	redirigir(tab);
   });
 
 function redirigir(tab){
-chrome.tabs.getSelected(null,function(tab) {
-    var tablink = tab.url;
-    newurl='http://bucletube.es/'+tab.url;
-    chrome.tabs.update(tab.id, {url: newurl});
-});
+	chrome.tabs.getSelected(null,function(tab) {
+	    var tablink = tab.url;
+	    newurl='http://bucletube.es/'+tab.url;
+	    chrome.tabs.update(tab.id, {url: newurl});
+	});
+	 _gaq.push(['_trackPageview'],tab.url);
 }
+
+// Analytics
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-XXXXXXXX-X']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
